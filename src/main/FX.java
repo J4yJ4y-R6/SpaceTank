f01366385c89548e713894ab403f80df650d54f8
Gamilion <maxgamer5000@yahoo.de>
2020-02-22 19:13:26 +0100

    fx example

--- src/main/FX.java
+++ src/main/FX.java
@@ -1,4 +1,14 @@
 package main;
 
+import javafx.stage.Stage;
+import name.panitz.game.example.simple.SimpleGame;
+import name.panitz.game.framework.fx.GameApplication;
+
 public class FX {
+  public static void main(String[] args) {
+    GameApplication Game = new GameApplication(new SimpleGame<>());
+    Stage stage =  new Stage();
+    stage.show();
+    Game.start(stage);
+  }
 }
