68d02614296521f70d760b49a2ff951587110447
JanielGamer <geisterhauserde@gmail.com>
2020-02-22 19:26:31 +0100

    added some frameworks

--- src/main/FX.java
+++ src/main/FX.java
@@ -5,7 +5,7 @@ import name.panitz.game.example.simple.SimpleGame;
 import name.panitz.game.framework.fx.GameApplication;
 
 public class FX {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     GameApplication Game = new GameApplication(new SimpleGame<>());
     Stage stage =  new Stage();
     stage.show();
